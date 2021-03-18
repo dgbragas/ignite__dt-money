4334afade7a2aec22e5ece2ecb7ef01cbbe6190c
dgbragas <dgbragas.dev@gmail.com>
2021-03-18 12:02:02 -0300

    feat: import created components in <App />

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+import { Dashboard } from './pages/Dashboard';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App(): JSX.Element {
-  return <GlobalStyle />;
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false,
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <GlobalStyle />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
+    </>
+  );
 }
