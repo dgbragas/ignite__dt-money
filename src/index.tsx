7faf2289a5cece8796765d99cc76ea51e02bfb9e
dgbragas <dgbragas.dev@gmail.com>
2021-03-18 15:02:53 -0300

    feat: config miragejs routes and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 03:00:01'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 09:00:33'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
-    // this.get('/transactions', (req, res) => {});
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   },
 });
 
