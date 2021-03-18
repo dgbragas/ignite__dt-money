746db2b50ab7be3055f1a789f85599731d75efad
dgbragas <dgbragas.dev@gmail.com>
2021-03-18 11:47:32 -0300

    feat: allow <Header /> to receive click property

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,20 @@ import logoImg from '../../assets/logo.svg';
 
 import * as S from './styles';
 
-export function Header(): JSX.Element {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({
+  onOpenNewTransactionModal,
+}: HeaderProps): JSX.Element {
   return (
     <S.Container>
       <S.Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </S.Content>
     </S.Container>
   );
