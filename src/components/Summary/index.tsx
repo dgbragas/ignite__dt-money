51f24806a19d3782a766080bc219e1acd5526cce
dgbragas <dgbragas.dev@gmail.com>
2021-03-19 16:45:53 -0300

    feat: calculate income, outcome and total card values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,12 +1,39 @@
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+
 import { SummaryCard } from './SummaryCard';
 import * as S from './styles';
 
 export function Summary(): JSX.Element {
+  const { transactions } = useContext(TransactionsContext);
+
+  const { deposits, total, withdraws } = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      }
+
+      if (transaction.type === 'withdraw') {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    },
+  );
+
   return (
     <S.Container>
-      <SummaryCard title="Entradas" type="income" value="R$1000,00" />
-      <SummaryCard title="Saídas" type="outcome" value="- R$500,00" />
-      <SummaryCard title="Total" type="total" value="R$500,00" />
+      <SummaryCard title="Entradas" type="income" value={deposits} />
+      <SummaryCard title="Saídas" type="outcome" value={withdraws} />
+      <SummaryCard title="Total" type="total" value={total} />
     </S.Container>
   );
 }
